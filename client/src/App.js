6fbe098b6b02d80a34107a31b99fa6a525952538
MacIsLyfe <mcooke1700@yahoo.com>
2023-10-03 20:44:36 -0400

    new code for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/user" component={UserPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+          {/* Add more routes for other pages */}
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function HomePage() {
+  return <div>Homepage</div>;
+}
+
+function UserPage() {
+  return <div>User</div>;
+}
+
+function LoginPage() {
+  return <div>Login</div>;
+}
+
+function RegisterPage() {
+  return <div>Register</div>;
 }
 
 export default App;
---
+++ client/src/Navbar.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+function Navbar() {
+  const [activeLink, setActiveLink] = useState('');
+
+  const handleNavLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/" onClick={() => handleNavLinkClick('home')}>Home</Link>
+        </li>
+        <li>
+          <Link to="/user" onClick={() => handleNavLinkClick('user')}>User</Link>
+        </li>
+        <li>
+          <Link to="/login" onClick={() => handleNavLinkClick('login')}>Login</Link>
+        </li>
+        <li>
+          <Link to="/register" onClick={() => handleNavLinkClick('register')}>Register</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
