dfcbdae6faf65591736b0d87d598ead481bd301d
Aaron <ajazzar@mail.usf.edu>
2023-10-05 23:03:53 -0400

    worked on  correcting the profile page some more

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,9 @@
-<<<<<<< HEAD
-// <<<<<<< HEAD
-// import './App.css';
-// import NavBar from './Navbar';
-// import { BrowserRouter, Route, Routes } from "react-router-dom"
-
-// function App() {
-//   return (
-//     <div className='App'>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path= "/" element={<NavBar/>}>
-//          </Route>
-//       </Routes>
-//     </BrowserRouter>
-//     </div>
-//   );
-// }
-
-// function HomePage() {
-//   return <div>Homepage</div>;
-// }
-
-// function UserPage() {
-//   return <div>Profile</div>;
-// }
-
-// function LoginPage() {
-//   return <div>Login</div>;
-// }
-
-// function SignUpPage() {
-//   return <div>Sign Up</div>;
-// }
-
-// export default App;
-// =======
-=======
->>>>>>> d0c68d8b401fc9d331a5d140a1bb08ff1ccb2491
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home";
 import Registration from "./react-register/register";
 import NotFound from "./NotFound";
 import Login from "./react-login/login";
-import RegistrationCard from "./react-profile/RegistrationCard";
+import Profile from "./react-profile/profile";
 import "./App.css";
 import { useEffect, useState } from "react";
 
@@ -91,7 +52,7 @@ function App() {
             element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
           />
           <Route path="/register" element={<Registration />} />
-          <Route path="/user" element={<RegistrationCard />} />
+          <Route path="/user" element={<Profile />} />
           <Route element={<NotFound />} />
         </Routes>
       </BrowserRouter>
@@ -100,7 +61,3 @@ function App() {
 }
 
 export default App;
-<<<<<<< HEAD
-// >>>>>>> abdc546ec4528167dda96a5180010bc48da81617
-=======
->>>>>>> d0c68d8b401fc9d331a5d140a1bb08ff1ccb2491
---
+++ client/src/react-profile/profile.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import ErrorAlert from "../layout/ErrorAlert";
+import { cancelRegistration } from "../utils/api";
+import RegistrationCard from "./RegistrationCard";
+
+export default function Profile({ user, handleCancel }) {
+  const [error, setError] = useState(null);
+  const history = useNavigate();
+
+  async function handleCancel(registrationId) {
+    if (
+      window.confirm(
+        "Do you want to delete your profile. This cannot be undone."
+      )
+    ) {
+      try {
+        await cancelRegistration(registrationId);
+        history.go();
+      } catch (error) {
+        setError(error);
+      }
+    }
+  }
+  return (
+    <div className="flex flex-col sm:flex-row sm:justify-center flex-wrap">
+      <ErrorAlert error={error} />
+      {user.map((registration) => (
+        <div key={registration.id}>
+          <RegistrationCard user={registration} handleCancel={handleCancel} />
+        </div>
+      ))}
+    </div>
+  );
+}
