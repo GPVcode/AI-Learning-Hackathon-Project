a73ba74d67da1bcc603cd2ae6d0cc639f7652347
MacIsLyfe <mcooke1700@yahoo.com>
2023-10-06 01:39:20 -0400

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,8 @@
-<<<<<<< HEAD
-import './App.css';
-import NavBar from './Navbar';
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-
-function App() {
-  return (
-    <div className='App'>
-    <BrowserRouter>
-      <Routes>
-        <Route path= "/" element={<NavBar/>}>
-         </Route>
-      </Routes>
-    </BrowserRouter>
-    </div>
-  );
-}
-
-function HomePage() {
-  return <div>Homepage</div>;
-}
-
-function UserPage() {
-  return <div>Profile</div>;
-}
-
-function LoginPage() {
-  return <div>Login</div>;
-}
-
-function SignUpPage() {
-  return <div>Sign Up</div>;
-}
-
-export default App;
-=======
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home";
 import Registration from "./react-register/register";
 import NotFound from "./NotFound";
+import NavBar from './Navbar';
 import Login from "./react-login/login";
 import "./App.css";
 import { useEffect, useState } from "react";
@@ -72,6 +37,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+        <NavBar/>
           <Route
             path="/"
             element={
@@ -95,4 +61,3 @@ function App() {
 }
 
 export default App;
->>>>>>> abdc546ec4528167dda96a5180010bc48da81617
