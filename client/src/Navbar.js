94df70be4474199ca905e7bc7c1e028b743af962
Aaron <ajazzar@mail.usf.edu>
2023-10-05 22:47:33 -0400

    worked on  correcting the profile page some more

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,32 +1,39 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom"
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 function Navbar() {
-  const [activeLink, setActiveLink] = useState('');
+  const [activeLink, setActiveLink] = useState("");
 
   const handleNavLinkClick = (link) => {
     setActiveLink(link);
   };
 
-
   return (
-    <nav className='navbar'>
-      <ul className='navbar-list'>
-        <li className='navbar-item'>
-          <Link to="/" onClick={() => handleNavLinkClick('home')}>Home</Link>
+    <nav className="navbar">
+      <ul className="navbar-list ">
+        <li className="navbar-item">
+          <Link to="/" onClick={() => handleNavLinkClick("home")}>
+            Home
+          </Link>
         </li>
-        <li className='navbar-item'>
-          <Link to="/user" onClick={() => handleNavLinkClick('profile')}>Profile</Link>
+        <li className="navbar-item">
+          <Link to="/user" onClick={() => handleNavLinkClick("profile")}>
+            Profile
+          </Link>
         </li>
-        <li className='navbar-item'>
-          <Link to="/login" onClick={() => handleNavLinkClick('login')}>Login</Link>
+        <li className="navbar-item">
+          <Link to="/login" onClick={() => handleNavLinkClick("login")}>
+            Login
+          </Link>
         </li>
-        <li className='navbar-item'>
-          <Link to="/signup" onClick={() => handleNavLinkClick('signup')}>Sign Up</Link>
+        <li className="navbar-item">
+          <Link to="/signup" onClick={() => handleNavLinkClick("signup")}>
+            Sign Up
+          </Link>
         </li>
       </ul>
     </nav>
   );
 }
 
-export default Navbar
+export default Navbar;
